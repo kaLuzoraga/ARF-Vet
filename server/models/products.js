6098b3c9c2260a809e935cf7395b661373e8e49e
kaLuzoraga <kurtluzoraga24@gmail.com>
2025-07-08 15:44:12 +0800

    User routes

---
+++ server/models/products.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0
+  }
+}, {
+  timestamps: true 
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
